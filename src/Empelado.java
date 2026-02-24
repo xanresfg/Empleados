4f61257c3c92efd175c4d48112def09a174d6ff4
Xandre <xanresfg@gmail.com>
2026-02-24 17:36:44 +0100

    first

---
+++ src/Empelado.java
@@ -0,0 +1,25 @@
+public class Empelado {
+    String nombre;
+    double salarioBase;
+
+    public Empelado(String nombre, double salarioBase) {
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+}
