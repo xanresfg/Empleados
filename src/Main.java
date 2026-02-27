9248a9a65dfbad2e0bf74e2601e74995ea8443a3
Xandre <xanresfg@gmail.com>
2026-02-27 18:55:09 +0100

    crear Empleados

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Empelado empleado1 = new Empelado("Juan", 1200.25);
+        Empelado programador1 = new Programador("María", 1200.25, "Java");
+        Empelado gerente1 = new Gerente("Pepe", 1200.25, "Marketing");
     }
 }
