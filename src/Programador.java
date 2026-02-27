24f6b9bc010c474e3de6d8647c334ac1e0b42c4a
Xandre <xanresfg@gmail.com>
2026-02-27 18:42:50 +0100

    calcularSalario programador

--- src/Programador.java
+++ src/Programador.java
@@ -7,6 +7,11 @@ public class Programador extends Empelado {
     }
 
     @Override
+    public double calcularSalario(){
+        return 110*super.salarioBase/100;
+    }
+
+    @Override
     public String toString(){
         return super.toString() + " Su lenguaje dominante es " + this.lenguajeDominante;
     }
